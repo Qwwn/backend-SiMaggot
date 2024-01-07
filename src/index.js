103e586d71d64170dad86d699eb436ffe1c15aea
Agyl Restu Hermanto <92309945+Qwwn@users.noreply.github.com>
2024-01-08 00:45:28 +0700

    update routes

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const cors = require('cors')
 const authRoutes = require('./routes/authRoute.js')
 const authenticate = require('./middleware/authenticate.js')
 const users = require('./api/user')
+const search = require('./api/search')
+const products = require('./api/product')
+const seller = require('./api/seller')
+const transaction = require('./api/transaction')
+const cart = require('./api/cart')
 
 const port = process.env.PORT || 8000
 app.listen(port, () => {
@@ -19,6 +24,11 @@ app.use(bodyParser.json())
 
 app.use('/auth', authRoutes)
 app.use('/user', users)
+app.use('/seller', seller)
+app.use('/search', search)
+app.use('/product', products)
+app.use('/cart', cart)
+app.use('/transaction', transaction)
 
 app.get('/protected', authenticate, (req, res) => {
   res.json({ message: 'This is a protected route', user: req.user })
